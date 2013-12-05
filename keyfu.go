3ee9a36056287ee31ca7a2cde5c1c4c32cde4527
Silas Sewell <silas@sewell.org>
2013-12-05 04:41:37 -0800

    Stub out basic run

--- keyfu.go
+++ keyfu.go
@@ -3,11 +3,18 @@ package main
 import (
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
+	"net/http"
+	"net/url"
 )
 
 func main() {
 	m := martini.Classic()
 	m.Use(render.Renderer("templates"))
+
+	m.Get("/run", func(res http.ResponseWriter, req *http.Request) {
+		q := req.FormValue("q")
+		http.Redirect(res, req, "https://www.google.com/search?q=" + url.QueryEscape(q), 302)
+	})
 
 	m.Get("/", func(r render.Render) {
 		r.HTML(200, "index", nil)
