8b3ae73a64ea857b42d0292debf808245bde35bc
Silas Sewell <silas@sewell.org>
2013-12-05 03:48:23 -0800

    Use martini

--- keyfu.go
+++ keyfu.go
@@ -1,32 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
 )
 
-func indexHandler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "<html><body><h1>KeyFu</h1></body></html>")
-}
-
-func handle() {
-	http.HandleFunc("/", indexHandler)
-}
-
-func serve() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "8000"
-	}
-
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		panic(err)
-	}
-}
-
 func main() {
-	handle()
-	serve()
+	m := martini.Classic()
+	m.Use(render.Renderer("templates"))
+
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", nil)
+	})
+
+	m.Run()
 }
