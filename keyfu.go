661031fa6ee80370e2c75b2218aadcbfe88e15f5
Silas Sewell <silas@sewell.org>
2013-12-07 19:26:08 -0800

    Remove templates and add link to code

--- keyfu.go
+++ keyfu.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"github.com/BurntSushi/toml"
 	"github.com/codegangsta/martini"
-	"github.com/codegangsta/martini-contrib/render"
 	"net/http"
 	"net/url"
 	"strings"
@@ -82,7 +81,6 @@ func main() {
 	}
 
 	m := martini.Classic()
-	m.Use(render.Renderer("templates"))
 
 	m.Get("/run", func(res http.ResponseWriter, req *http.Request) {
 		q := req.FormValue("q")
@@ -91,10 +89,6 @@ func main() {
 		} else {
 			http.Redirect(res, req, "https://www.google.com/search?q="+url.QueryEscape(q), 302)
 		}
-	})
-
-	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", nil)
 	})
 
 	m.Run()
