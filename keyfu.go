819f9570e2bc0cb9a3a1816188deb098e32e2beb
Silas Sewell <silas@sewell.org>
2013-12-07 19:19:08 -0800

    Config keywords via toml

--- keyfu.go
+++ keyfu.go
@@ -1,13 +1,86 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"github.com/BurntSushi/toml"
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	"net/http"
 	"net/url"
+	"strings"
+	"unicode"
 )
 
+type Keyword struct {
+	Url      string `toml:"url"`
+	QueryUrl string `toml:"query_url"`
+}
+
+type Config struct {
+	Keywords map[string]Keyword `toml:"keyword"`
+}
+
+var config Config
+
+func parseQ(value string) (string, string) {
+	if value == "" {
+		return "", ""
+	}
+
+	begin := -1
+	end := len(value)
+	for i, r := range value {
+		if !unicode.IsSpace(r) {
+			if begin == -1 {
+				begin = i
+			}
+		} else if begin != -1 {
+			end = i
+			break
+		}
+	}
+
+	if begin == -1 {
+		return "", ""
+	}
+
+	return value[begin:end], strings.TrimLeftFunc(value[end:], unicode.IsSpace)
+}
+
+func Run(q string) (string, error) {
+	key, value := parseQ(q)
+	k, ok := config.Keywords[key]
+
+	if !ok {
+		return "", errors.New("not found")
+	}
+
+	var u string
+
+	if len(value) > 0 {
+		if len(k.QueryUrl) == 0 {
+			return "", errors.New("no query url for keyword")
+		}
+		u = k.QueryUrl
+	} else {
+		if len(k.Url) == 0 {
+			return "", errors.New("no url for keyword")
+		}
+		u = k.Url
+	}
+
+	return strings.Replace(u, "%s", url.QueryEscape(value), -1), nil
+}
+
 func main() {
+	var configPath = flag.String("c", "keyfu.conf", "KeyFu configuration file")
+	flag.Parse()
+
+	if _, err := toml.DecodeFile(*configPath, &config); err != nil {
+		panic(err)
+	}
+
 	m := martini.Classic()
 	m.Use(render.Renderer("templates"))
 
