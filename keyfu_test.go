c33b2fac964c874fccf8ac03e612bff74791ad3c
Silas Sewell <silas@sewell.org>
2014-01-03 00:35:36 -0800

    Add simple run test

--- keyfu_test.go
+++ keyfu_test.go
@@ -21,3 +21,48 @@ func TestParseQ(t *testing.T) {
 	assertParseQ(t, "one two three", "one", "two three")
 	assertParseQ(t, "one  two  three ", "one", "two  three ")
 }
+
+func server() *Server {
+	s := Server{}
+
+	s.Keywords = map[string]Keyword{
+		"one":  Keyword{"one", ""},
+		"two":  Keyword{"", "two=%s"},
+		"both": Keyword{"url", "query_url=%s"},
+	}
+
+	return &s
+}
+
+func assertRun(t *testing.T, s *Server, q, url string) {
+	u, err := s.Run(q)
+	if assert.Nil(t, err) {
+		assert.Equal(t, u, url)
+	}
+}
+
+func assertNotRun(t *testing.T, s *Server, q string) {
+	_, err := s.Run(q)
+	assert.NotNil(t, err)
+}
+
+func TestRun(t *testing.T) {
+	s := server()
+
+	// not found
+	assertNotRun(t, s, "")
+	assertNotRun(t, s, "notfound")
+
+	// only url
+	assertRun(t, s, "one", "one")
+	assertNotRun(t, s, "one a")
+
+	// only query_url
+	assertRun(t, s, "two 2", "two=2")
+	assertNotRun(t, s, "two")
+
+	// url and query_url
+	assertRun(t, s, "both a", "query_url=a")
+	assertRun(t, s, "both a", "query_url=a")
+	assertRun(t, s, "both a b", "query_url=a+b")
+}
