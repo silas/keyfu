923b584d96a2fb4171c47e1e0fe8d56f972851c8
Silas Sewell <silas@sewell.org>
2014-01-25 14:03:33 -0800

    Add test server

--- keyfu_test.go
+++ keyfu_test.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -101,4 +106,62 @@ func TestRequest(t *testing.T) {
 		assert.Equal(t, req.Key, "example")
 		assert.Equal(t, req.Value, "one two")
 	}
+}
+
+func run(t *testing.T, ts *httptest.Server, q string) (url, body string) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s?q="+q, ts.URL), nil)
+	if !assert.Nil(t, err) {
+		return
+	}
+
+	tr := &http.Transport{}
+	res, err := tr.RoundTrip(req)
+	if assert.Nil(t, err) && assert.NotNil(t, res) {
+		defer res.Body.Close()
+		b, err := ioutil.ReadAll(res.Body)
+		if assert.Nil(t, err) {
+			return res.Header.Get("Location"), string(b)
+		}
+	}
+
+	return
+}
+
+func assertRunLocation(t *testing.T, ts *httptest.Server, q, value string) {
+	location, _ := run(t, ts, q)
+	assert.Equal(t, location, value)
+}
+
+func assertRunBody(t *testing.T, ts *httptest.Server, q, value string) {
+	_, body := run(t, ts, q)
+	assert.Equal(t, body, value)
+}
+
+func TestServer(t *testing.T) {
+	os.Setenv("HOST", "")
+	os.Setenv("PORT", "")
+
+	s := Server{}
+	err := s.Init("./test/keyfu.conf")
+	if assert.Nil(t, err) {
+		assert.Equal(t, s.Config.Listen, ":8000")
+	}
+
+	os.Setenv("HOST", "0.0.0.0")
+	os.Setenv("PORT", "1234")
+
+	s = Server{}
+	err = s.Init("./test/keyfu.conf")
+	if assert.Nil(t, err) {
+		assert.Equal(t, s.Config.Listen, "0.0.0.0:1234")
+	}
+
+	ts := httptest.NewServer(http.HandlerFunc(s.RunHandler))
+	defer ts.Close()
+
+	assertRunLocation(t, ts, "hello+world", "https://encrypted.google.com/search?q=hello+world")
+	assertRunLocation(t, ts, "gh", "https://github.com/")
+	assertRunLocation(t, ts, "gh+code", "https://github.com/search?q=code")
+	assertRunLocation(t, ts, "redirect", "http://www.keyfu.com/")
+	assertRunBody(t, ts, "render", "hello world\n")
 }
